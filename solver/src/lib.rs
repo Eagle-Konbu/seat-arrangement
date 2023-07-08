2805f66e55dce69f5de9779b80df89ce9fcc55ca
Atsuya Uchida <atsuya_eagle@outlook.com>
2023-07-08 22:45:46 +0900

    Defined structs.

--- solver/src/lib.rs
+++ solver/src/lib.rs
@@ -2,6 +2,26 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+struct Input {
+    students: Vec<Student>,
+    row: usize,
+    column: usize,
+}
+
+struct Student {
+    name: String,
+    academic_ability: usize,
+    exercise_ability: usize,
+    leadership: usize,
+    needs_assistance: bool,
+    gender: Gender,
+}
+
+enum Gender {
+    Male,
+    Female
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
