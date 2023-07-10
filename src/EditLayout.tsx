8aba08b846a466fead5b00296890127144f261de
Atsuya Uchida <atsuya_eagle@outlook.com>
2023-07-10 14:25:41 +0900

    Modified EditLayout.tsx.`

--- src/EditLayout.tsx
+++ src/EditLayout.tsx
@@ -1,57 +1,32 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
 import { useSearchParams } from "react-router-dom";
+import { Card, Grid } from "@mui/material";
 
 function EditLayout() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
 
   const [searchParams, _] = useSearchParams();
-  
+
   const width = Number(searchParams.get("width"));
   const depth = Number(searchParams.get("depth"));
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
+    <div>
+      <Grid container spacing={2} columns={width}>
+        {(() => {
+          const elements = [];
+          for (let i = 0; i < width * depth; i++) {
+            elements.push(
+              <Grid item xs={1}>
+                <Card>
+                  aaa
+                </Card>
+              </Grid>
+            );
+          }
+          return elements;
+        })()}
+      </Grid>
     </div>
   );
 }
