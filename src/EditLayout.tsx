f02ad7bc6b4143d64dc2194b963b4309faea6530
Atsuya Uchida <atsuya_eagle@outlook.com>
2023-07-12 15:48:54 +0900

    Added drawer.

--- src/EditLayout.tsx
+++ src/EditLayout.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 import { useSearchParams } from "react-router-dom";
-import { Card, CardContent, Grid, Typography } from "@mui/material";
+import { Card, CardContent, Drawer, FormControl, Grid, InputLabel, TextField, Typography } from "@mui/material";
 import SeatCard from "./components/SeatCard";
 
 function EditLayout() {
@@ -11,7 +11,13 @@ function EditLayout() {
   const width = Number(searchParams.get("width"));
   const depth = Number(searchParams.get("depth"));
 
+  const [drawerIsOpen, setDrawerIsOpen] = useState(true);
+
   const sampleStudent = { id: 1, name: "田中 太郎", academic_ability: 3, exercise_ability: 3, leadership_ability: 3, needs_assistance: false, gender: "Male" };
+
+  function toggleDrawer() {
+    setDrawerIsOpen(!drawerIsOpen);
+  }
 
   return (
     <div>
@@ -26,6 +32,16 @@ function EditLayout() {
           return elements;
         })()}
       </Grid>
+
+      <Drawer
+        anchor="right"
+        open={drawerIsOpen}
+      >
+        <FormControl fullWidth>
+          <InputLabel id="student_name">名前</InputLabel>
+          <TextField id="student_name" label="名前" variant="standard" />
+        </FormControl>
+      </Drawer>
     </div>
   );
 }
