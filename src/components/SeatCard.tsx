5579fae6df0f4e48e92787d8afe1a107b0bebcf2
Atsuya Uchida <atsuya_eagle@outlook.com>
2023-07-13 08:59:48 +0900

    Added tooltip for SeatCard.

--- src/components/SeatCard.tsx
+++ src/components/SeatCard.tsx
@@ -1,17 +1,26 @@
-import { Card, CardActionArea, CardContent, Grid, Typography } from "@mui/material";
+import { Card, CardActionArea, CardContent, Grid, Tooltip, Typography } from "@mui/material";
 
 function SeatCard(props: { student: Student, onClick: () => void }) {
+  const studentInfo = (student: Student) => {
+    let res = `学力: ${student.academic_ability} 運動能力: ${student.exercise_ability} リーダーシップ: ${student.leadership_ability}`
+    if (student.needs_assistance) {
+      res += " 要支援"
+    }
+    return res;
+  };
   return (
     <Grid item xs={1}>
-      <Card variant="outlined">
-        <CardActionArea onClick={props.onClick}>
-          <CardContent sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <Typography>
-              {props.student.id}. {props.student.name}
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
+      <Tooltip title={studentInfo(props.student)} arrow>
+        <Card variant="outlined">
+          <CardActionArea onClick={props.onClick}>
+            <CardContent sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+              <Typography>
+                {props.student.id}. {props.student.name}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Tooltip>
     </Grid>
   );
 }
