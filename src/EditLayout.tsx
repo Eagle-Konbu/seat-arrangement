2c6a7928db019da06aab54a8a7653baf91641b16
Atsuya Uchida <atsuya_eagle@outlook.com>
2023-07-13 09:39:43 +0900

    Added data.

--- src/EditLayout.tsx
+++ src/EditLayout.tsx
@@ -5,6 +5,7 @@ import { Box, Drawer, Grid, Stack, TextField, Slider, Divider, Typography, Input
 import SeatCard from "./components/SeatCard";
 
 function EditLayout() {
+  const sampleStudent = { id: 1, name: "田中 太郎", academic_ability: 3, exercise_ability: 3, leadership_ability: 3, needs_assistance: false, gender: "Male" };
 
   const [searchParams, _] = useSearchParams();
 
@@ -13,10 +14,29 @@ function EditLayout() {
 
   const [drawerIsOpen, setDrawerIsOpen] = useState(false);
 
-  const sampleStudent = { id: 1, name: "田中 太郎", academic_ability: 3, exercise_ability: 3, leadership_ability: 3, needs_assistance: false, gender: "Male" };
+  const [editedPosition, setEditedPosition] = useState([-1, -1]);
+  const [editedStudent, setEditedStudent] = useState(sampleStudent);
+
+  const [seats, setSeats] = useState<(Student | null)[][]>(() => {
+    const seats = [];
+    for (let i = 0; i < depth; i++) {
+      const row = [];
+      for (let j = 0; j < width; j++) {
+        row.push(null);
+      }
+      seats.push(row);
+    }
+    return seats;
+  });
 
   function toggleDrawer() {
     setDrawerIsOpen(!drawerIsOpen);
+  }
+
+  function setStudent(row: number, col: number, id: number, name: string, academic_ability: number, exercise_ability: number, leadership_ability: number, needs_assistance: boolean, gender: string) {
+    const newSeats = [...seats];
+    newSeats[row][col] = { id, name, academic_ability, exercise_ability, leadership_ability, needs_assistance, gender };
+    setSeats(newSeats);
   }
 
   return (
@@ -26,8 +46,16 @@ function EditLayout() {
           {(() => {
             const elements = [];
             for (let i = 0; i < width * depth; i++) {
+              let x = i % width;
+              let y = Math.floor(i / width);
               elements.push(
-                <SeatCard student={sampleStudent} onClick={toggleDrawer} />
+                <SeatCard
+                  student={sampleStudent}
+                  onClick={() => {
+                    setEditedPosition([x, y]);
+                    toggleDrawer();
+                  }}
+                />
               );
             }
             return elements;
@@ -41,10 +69,10 @@ function EditLayout() {
         open={drawerIsOpen}
       >
         <Box padding={2}>
-          <TextField label="出席番号" type="number" defaultValue={sampleStudent.id} margin="normal" />
+          <TextField label="出席番号" type="number" margin="normal" />
           <Divider />
 
-          <TextField label="名前" defaultValue={sampleStudent.name} margin="normal" />
+          <TextField label="名前" margin="normal" />
           <Divider />
 
           <InputLabel id="gender-select">性別</InputLabel>
@@ -58,7 +86,6 @@ function EditLayout() {
 
           <Typography gutterBottom>学力</Typography>
           <Slider
-            defaultValue={3}
             step={1}
             max={5}
             min={1}
@@ -69,7 +96,6 @@ function EditLayout() {
 
           <Typography gutterBottom>運動能力</Typography>
           <Slider
-            defaultValue={3}
             step={1}
             max={5}
             min={1}
@@ -80,7 +106,6 @@ function EditLayout() {
 
           <Typography gutterBottom>リーダーシップ</Typography>
           <Slider
-            defaultValue={3}
             step={1}
             max={5}
             min={1}
@@ -90,7 +115,8 @@ function EditLayout() {
           <Divider />
 
           <Typography gutterBottom>支援が必要</Typography>
-          <Checkbox />
+          <Checkbox
+          />
           <Divider />
 
           <Stack direction="row" spacing={2}>
