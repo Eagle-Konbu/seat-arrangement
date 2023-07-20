b6db8a1dea030ee8606a5ecc7173d14fb037513e
Atsuya Uchida <atsuya_eagle@outlook.com>
2023-07-20 09:49:15 +0900

    Added pdf-generator.

---
+++ pdf-generator/src/lib.rs
@@ -0,0 +1,18 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+pub fn gen(seats: Vec<Vec<String>>) {
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
