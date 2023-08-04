1109bace2a18b150d1b7db8b69b5ac19663b6e27
Atsuya Uchida <atsuya_eagle@outlook.com>
2023-08-04 09:08:47 +0900

    Implemented tabu search.

--- solver/src/tabu_search.rs
+++ solver/src/tabu_search.rs
@@ -4,9 +4,8 @@ use crate::{
     utils::swap_seats,
 };
 
-use std::io::Error;
+use std::{collections::{BTreeSet, VecDeque}, io::Error};
 
-//TODO: 移動させて，離れた生徒同士のペアをタブーリストに入れる
 pub fn execute<R: rand::Rng>(
     previous: &SeatAssignment,
     students: &[Student],
@@ -15,5 +14,41 @@ pub fn execute<R: rand::Rng>(
     tabu_list_size: usize,
     rng: &mut R,
 ) -> Result<(SeatAssignment, i64), Error> {
+    let mut tabu_list = BTreeSet::new();
+    let mut deq = VecDeque::new();
 
+    let mut current = previous.clone();
+
+    let mut best = evaluate(previous, &current, students).unwrap();
+    
+    for _ in 0..loop_cnt {
+        for _ in 0..neighbor_cnt {
+            let (pos1, pos2) = loop {
+                let pos1 = (rng.gen_range(0..previous.len()), rng.gen_range(0..previous[0].len()));
+                let pos2 = (rng.gen_range(0..previous.len()), rng.gen_range(0..previous[0].len()));
+                if pos1 != pos2 && !tabu_list.contains(&(students[previous[pos1.0][pos1.1]].id, students[previous[pos2.0][pos2.1]].id)) {
+                    break (pos1, pos2);
+                }
+            };
+
+            swap_seats(&mut current, pos1, pos2);
+            let score = evaluate(previous, &current, students).unwrap();
+            if score < best {
+                best = score;
+            } else {
+                swap_seats(&mut current, pos1, pos2);
+                continue;
+            }
+
+            tabu_list.insert((students[previous[pos1.0][pos1.1]].id, students[previous[pos2.0][pos2.1]].id));
+            deq.push_back((students[previous[pos1.0][pos1.1]].id, students[previous[pos2.0][pos2.1]].id));
+
+            while deq.len() > tabu_list_size {
+                let (id1, id2) = deq.pop_front().unwrap();
+                tabu_list.remove(&(id1, id2));
+            }
+        }
+    }
+
+    Ok((current, best))
 }
